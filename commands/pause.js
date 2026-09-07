69584151525451d71f34a7aa326aa2e22e2f2a18
sushithmuthineni-lgtm <sushithmuthineni@gmail.com>
2026-09-07 20:09:14 +0530

    Clean up comments in pause.js
    Removed old ASCII art and contact information from the pause.js file.

--- commands/pause.js
+++ commands/pause.js
@@ -1,17 +1,10 @@
 /*
 
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+      ██████╗   █████╗   ██╗     ██╗          ███████╗   ████████╗
+       ██╔══██╗ ██╔══██╗ ██║     ██║         ██╔════╝   ╚══██╔══╝
+       ██████╔╝ ███████║ ██║     ██║         █████╗         ██║   
+       ██╔══██╗ ██╔══██║ ██║     ██║         ██╔══╝         ██║   
+       ╚═════╝  ╚═╝  ╚═╝  ╚══════╝╚══════╝  ╚══════╝      ╚═╝   
 */
 const db = require("../mongoDB");
 const { EmbedBuilder } = require('discord.js');
@@ -51,16 +44,10 @@ module.exports = {
 
 /*
 
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
+        ██████╗   █████╗  ██╗     ██╗          ███████╗   ████████╗
+       ██╔══██╗ ██╔══██╗ ██║     ██║         ██╔════╝   ╚══██╔══╝
+       ██████╔╝ ███████║ ██║     ██║         █████╗         ██║   
+       ██╔══██╗ ██╔══██║ ██║     ██║         ██╔══╝         ██║   
+       ╚═════╝  ╚═╝  ╚═╝  ╚══════╝╚══════╝  ╚══════╝      ╚═╝   
 
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
 */
