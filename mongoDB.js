966ec61b464d6f74bec5e2b67b1ff6ff96e97c15
sushithmuthineni-lgtm <sushithmuthineni@gmail.com>
2026-09-07 20:26:49 +0530

    Clean up comments and banners in mongoDB.js
    Removed old banner and contact information from the file.

--- mongoDB.js
+++ mongoDB.js
@@ -1,17 +1,10 @@
 /*
 
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+      ██████╗   █████╗   ██╗     ██╗          ███████╗   ████████╗
+       ██╔══██╗ ██╔══██╗ ██║     ██║         ██╔════╝   ╚══██╔══╝
+       ██████╔╝ ███████║ ██║     ██║         █████╗         ██║   
+       ██╔══██╗ ██╔══██║ ██║     ██║         ██╔══╝         ██║   
+       ╚═════╝  ╚═╝  ╚═╝  ╚══════╝╚══════╝  ╚══════╝      ╚═╝   
 */
 const { Schema, model } = require("mongoose");
 
@@ -36,16 +29,9 @@ module.exports = {
 };
 /*
 
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+       ██████╗   █████╗   ██╗     ██╗          ███████╗   ████████╗
+       ██╔══██╗ ██╔══██╗ ██║     ██║         ██╔════╝   ╚══██╔══╝
+       ██████╔╝ ███████║ ██║     ██║         █████╗         ██║   
+       ██╔══██╗ ██╔══██║ ██║     ██║         ██╔══╝         ██║   
+       ╚═════╝  ╚═╝  ╚═╝  ╚══════╝╚══════╝  ╚══════╝      ╚═╝   
 */
