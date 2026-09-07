bc9c668a09260ec9f2656c390ffd4587db946a5b
sushithmuthineni-lgtm <sushithmuthineni@gmail.com>
2026-09-07 20:07:42 +0530

    Clean up loop.js by removing old comments
    Removed old banner and contact information from loop.js.

--- commands/loop.js
+++ commands/loop.js
@@ -1,17 +1,10 @@
 /*
 
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+       ██████╗   █████╗   ██╗     ██╗          ███████╗   ████████╗
+       ██╔══██╗ ██╔══██╗ ██║     ██║         ██╔════╝   ╚══██╔══╝
+       ██████╔╝ ███████║ ██║     ██║         █████╗         ██║   
+       ██╔══██╗ ██╔══██║ ██║     ██║         ██╔══╝         ██║   
+       ╚═════╝  ╚═╝  ╚═╝  ╚══════╝╚══════╝  ╚══════╝      ╚═╝   
 */
 const db = require("../mongoDB");
 module.exports = {
@@ -109,16 +102,9 @@ module.exports = {
 }
 /*
 
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+      ██████╗   █████╗   ██╗     ██╗          ███████╗   ████████╗
+       ██╔══██╗ ██╔══██╗ ██║     ██║         ██╔════╝   ╚══██╔══╝
+       ██████╔╝ ███████║ ██║     ██║         █████╗         ██║   
+       ██╔══██╗ ██╔══██║ ██║     ██║         ██╔══╝         ██║   
+       ╚═════╝  ╚═╝  ╚═╝  ╚══════╝╚══════╝  ╚══════╝      ╚═╝   
 */
